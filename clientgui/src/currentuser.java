38a55929a70e45b99914db5180001d73381455a8
gurkih <markus.fritscher@fau.de>
2015-09-25 11:40:55 +0000

    tool now reads the cardid via the cardreader and connects to the djangodb running on ws01 to request the users permissions

--- clientgui/src/currentuser.java
+++ clientgui/src/currentuser.java
@@ -1,10 +1,10 @@
 
-
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
-
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -17,73 +17,124 @@ public class currentuser {
 	private boolean einweisung = false;
 	private String fauid = "";
 	private boolean error;
-	
+
 	public boolean getBetreuer() {
 		return this.betreuer;
 	}
-	
+
 	public boolean getEinweisung() {
 		return this.einweisung;
 	}
-	
+
 	public String getFauId() {
 		return this.fauid;
 	}
-	
+
 	public boolean getError() {
 		return this.error;
 	}
-	
+
+	public String getcardid() {
+		// partly taken from http://alvinalexander.com/java/edu/pj/pj010016
+		
+		// were running the getuid-pcsc tool from the command line to read the cardid from the cardreader
+		
+		String s = null;
+		String retstring = "";
+
+		try {
+			Process p = Runtime.getRuntime().exec("./getuid-pcsc");
+
+			BufferedReader stdInput = new BufferedReader(new InputStreamReader(
+					p.getInputStream()));
+
+			BufferedReader stdError = new BufferedReader(new InputStreamReader(
+					p.getErrorStream()));
+
+			// read the output from the command
+			if ((s = stdInput.readLine()) != null) {
+				retstring += s;
+
+			}
+
+			if (retstring.contains("SCard")) {
+				return "error";
+			}
+
+			// read any errors from the attempted command
+			while ((s = stdError.readLine()) != null) {
+				System.out
+						.println("Here is the standard error of the command (if any):\n");
+				System.out.println(s);
+			}
+			return retstring;
+		}
+
+		catch (IOException e) {
+			System.out.println("exception happened - here's what I know: ");
+			//e.printStackTrace();
+			return ("error");
+		}
+	}
+
 	private String convertStreamToString(java.io.InputStream is) {
 		java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
 		return s.hasNext() ? s.next() : "";
 	}
-	
+
 	private void checkForEinweisung(String fauid, String Geraet) {
 		this.error = false;
 		try {
-			// TODO: change the way i read the json to https://docs.oracle.com/javase/tutorial/networking/urls/readingURL.html
-			
-			// or: http://stackoverflow.com/questions/9856195/how-to-read-an-http-input-stream
-			
-			String urlstring = "http://localhost:8000/users/";
-			urlstring += fauid;
-			URL myurl = new URL(urlstring);
-			InputStream is = myurl.openStream();
-			// see
-			// http://stackoverflow.com/questions/309424/read-convert-an-inputstream-to-a-string
-			String myInputString = convertStreamToString(is);
-			// System.out.println(myInputString);
-			JSONParser parser = new JSONParser();
-			Object myObj = parser.parse(myInputString);
-			
-			JSONArray myJsonObj = (JSONArray) myObj;
-			JSONObject myParsedJsonObj = (JSONObject)((JSONObject) myJsonObj.get(0)).get("fields");
+			String myCardId = getcardid();
+			if (myCardId.equals("error") == false) {
 
-			// http://examples.javacodegeeks.com/core-java/json/java-json-parser-example/
-			
-			if ((boolean) myParsedJsonObj.get(Geraet)) {
-				this.einweisung = true;
+				String urlstring = "http://ws01:8000/users/";
+				urlstring += myCardId;
+				// System.out.println(urlstring);
+				URL myurl = new URL(urlstring);
+				InputStream is = myurl.openStream();
+				// see
+				// http://stackoverflow.com/questions/309424/read-convert-an-inputstream-to-a-string
+				String myInputString = convertStreamToString(is);
+				// System.out.println(myInputString);
+				JSONParser parser = new JSONParser();
+				Object myObj = parser.parse(myInputString);
+
+				JSONArray myJsonObj = (JSONArray) myObj;
+				JSONObject myParsedJsonObj = (JSONObject) ((JSONObject) myJsonObj
+						.get(0)).get("fields");
+
+				// http://examples.javacodegeeks.com/core-java/json/java-json-parser-example/
+
+				if ((boolean) myParsedJsonObj.get(Geraet)) {
+					this.einweisung = true;
+				}
+				if ((boolean) myParsedJsonObj.get("Betreuer")) {
+					this.betreuer = true;
+				}
+				this.fauid = myParsedJsonObj.get("fauid").toString();
+			} else {
+				this.einweisung = false;
+				this.betreuer = false;
+				this.fauid = "unknown";
 			}
-			if ((boolean) myParsedJsonObj.get("Betreuer")) {
-				this.betreuer = true;
-			}
-			this.fauid = myParsedJsonObj.get("fauid").toString();
 
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
-			System.out.println("Either the fauid " + fauid + " is unknown or there is a problem with your network.");
+			System.out.println("Either the fauid " + fauid
+					+ " is unknown or there is a problem with your network.");
 			this.error = true;
 		} catch (ParseException e) {
 			e.printStackTrace();
 		} catch (java.lang.NullPointerException e) {
-			System.out.println("Theese arent the droids you're looking for. Check with your local Django to add "
-					+ Geraet + " to the list of supported devices.");
+			System.out
+					.println("Theese arent the droids you're looking for. Check with your local Django to add "
+							+ Geraet + " to the list of supported devices.");
 			this.error = true;
 		}
 	}
-	
+
 	public currentuser(String fauid, String myGeraet) {
 		this.checkForEinweisung(fauid, myGeraet);
 	}
